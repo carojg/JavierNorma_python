ff37f2c34003fdeae0770bb9ac2ce07314f4d9a6
carojg <normajg4474@gmail.com>
2019-05-11 20:58:49 -0500

    actualizacionFuncion

--- PaisesConsulta/apiCapitales.py
+++ PaisesConsulta/apiCapitales.py
@@ -9,23 +9,33 @@ import requests
 import json
 from IPython.display import SVG, display
 
+def peticionApi(pais):
+    # Creamos la petición HTTP con GET:
+    URL = "https://restcountries.eu/rest/v2/"#Guardamos la url
+    PARAMS = "name/"+ pais #Guardamos los parametros
+    resp = requests.get(URL + PARAMS)#Solicitamos los datos
+
+    if (resp.status_code == 200): #Verificamos que tenga datos
+        txt = resp.text #Lo guardamos en una variable
+
+        if (txt[0] == "["): #Si el primer caracter es [ entonces eliminamos el primero y el ultimo
+            txt = txt[ 1:len(txt) - 1]
+
+        objDicc = json.loads(txt) #Cargamos el archivo en formato json
+
+        return objDicc;
+    else:
+        return "nada"
+
 #Capturamos los datos
 pais = input("Introduce el nombre del país que requiera buscar: ")
-
 print ("La cadena que ingreso es: ",pais, "\n")#Imprimimos el dato ingresado
-# Creamos la petición HTTP con GET:
-URL = "https://restcountries.eu/rest/v2/"#Guardamos la url
-PARAMS = "name/"+ pais #Guardamos los parametros
-resp = requests.get(URL + PARAMS)#Solicitamos los datos
 
-if (resp.status_code == 200): #Verificamos que tenga datos
-    txt = resp.text #Lo guardamos en una variable
+obj = peticionApi(pais); #Mandamos a llamar a la función para conectarnos a la api
+#Esta función nos retorna un diccionario
+#print(type(obj))
 
-    if (txt[0] == "["): #Si el primer caracter es [ entonces eliminamos el primero y el ultimo
-        txt = txt[ 1:len(txt) - 1]
-    
-    obj = json.loads(txt) #Cargamos el archivo en formato json
-    
+if (obj != "nada"):
     urlImg = obj["flag"] #Extraemos la imagen
 
     #Imprimimos los datos
@@ -34,6 +44,8 @@ if (resp.status_code == 200): #Verificamos que tenga datos
           "Habitantes: ", obj["population"], "\n",
           "Bandera (url): ", urlImg, "\n"
          )
-    
-    display(SVG(url=urlImg)) #Imprimimos la imagen    
+
+    display(SVG(url=urlImg)) #Imprimimos la imagen  
+else:
+    print("Ese pais no existe!")
 
